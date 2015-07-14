e30ff646dc89ba18951e3e54e774d2776974cc01
antoniom2015 <ieje@live.com>
2015-07-14 12:53:51 +0200

    varios cambios realizados en varios archivos y poner un favicon

---
+++ app.js
@@ -0,0 +1,62 @@
+//importar paquetes con middlewares
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//importar enrutadores
+var routes = require('./routes/index');
+//var users = require('./routes/users');
+//crear aplicación
+var app = express();
+
+// view engine setup instalar generador de vistas ejs, no jade
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', '/images/favicon.ico')));
+//instalar middlewares 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//instalar enrutadores
+app.use('/', routes);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler //resto de rutas genera error 404 de HTTP
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace //gestión de errores durante el desarrollo
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user //gestión de errores de producción
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+//exportar app para comando de arranque
+module.exports = app;
